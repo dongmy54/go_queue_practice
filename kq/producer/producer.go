1838dc19535fd0395e215384c481b28455961585
dongmingyan <dongmingyan01@gmail.com>
2024-10-28 11:28:07 +0800

    update

--- kq/producer/producer.go
+++ kq/producer/producer.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/segmentio/kafka-go"
 	"github.com/zeromicro/go-queue/kq"
 	"github.com/zeromicro/go-zero/core/cmdline"
 )
@@ -24,7 +25,8 @@ func main() {
 		"127.0.0.1:9092",
 		"127.0.0.1:9092",
 		"127.0.0.1:9092",
-	}, "kq")
+	}, "kq", kq.WithBalancer(&kafka.Hash{}))
+	// kq.WithBalancer(&kafka.Hash{}) 主要是用于相同的key进入同一个分区中
 
 	ticker := time.NewTicker(time.Millisecond)
 	for round := 0; round < 3; round++ {
